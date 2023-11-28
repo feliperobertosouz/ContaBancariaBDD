1187b6718c708e15d6ecb031010181b676bdbc73
Felipe Roberto <56833987+feliperobertosouz@users.noreply.github.com>
2023-11-28 20:13:41 -0300

    feat: added runner class + added junity4 cucumber into pom.xml

---
+++ src/test/java/Runner.java
@@ -0,0 +1,10 @@
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test",
+        glue = {"org.example"})
+public class Runner {
+}
